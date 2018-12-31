bc55d67fe83dd291aab8b9bca154fb1d39647be7
SamanthaChia <samantha1999@live.com.sg>
2018-12-31 13:05:09 +0800

    python strings

---
+++ name.py
@@ -0,0 +1,10 @@
+# name = "samantha chia eileen"
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+
+first_name = "samantha"
+last_name = "chia"
+full_name = first_name + " " + last_name
+# print(full_name)
+print("Hello, " + full_name.title() + "!")
