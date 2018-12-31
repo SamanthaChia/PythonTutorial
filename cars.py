44dafe9feb714a72326b542564a895b2655877bc
SamanthaChia <samantha1999@live.com.sg>
2018-12-31 15:42:04 +0800

    sorting of index in a list

---
+++ cars.py
@@ -0,0 +1,16 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+# sort by ascending alphabet. (?)
+
+cars_reverse_true = ['bmw', 'audi', 'toyota', 'subaru']
+cars_reverse_true.sort(reverse=True)
+print(cars_reverse_true)
+# sort descending alphabet (?)
+
+cars_reverse = ['bmw', 'audi', 'toyota', 'subaru']
+cars_reverse.reverse()
+print(cars_reverse)
+
+len(cars)
+# in the terminal , to get the length of the list.
