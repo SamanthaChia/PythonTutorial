455872c8818a38268468ebdb9a7f46bef6819632
SamanthaChia <samantha1999@live.com.sg>
2018-12-31 14:48:22 +0800

    apostrophes and white_space functions

---
+++ white_space.py
@@ -0,0 +1,17 @@
+print("Python")
+
+print("\tPython2")
+
+print("Languages:\nPython\nC\nJavascript")
+
+favourite_language = 'python '
+print(favourite_language)
+
+favourite_language.rstrip()
+# remove white space from the last letter
+
+favourite_language.lstrip()
+#remove white space from the starting leter
+
+favourite_language.strip()
+#remove space from front and back part of the string.
