a271384e9040a2136d61452d64404903f4f08c4a
SamanthaChia <samantha1999@live.com.sg>
2018-12-31 15:14:14 +0800

    lists with python

---
+++ bicycles.py
@@ -0,0 +1,9 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title())
+
+print(bicycles[-1])
+#the index before trek would be specialized.
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+
+print(message)
