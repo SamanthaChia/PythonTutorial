b3bc6e5d867e37bd3003f7f5db92e0c44dbd5209
SamanthaChia <samantha1999@live.com.sg>
2019-01-02 19:22:54 +0800

    loops in dictionaries

--- favorite_languages.py
+++ favorite_languages.py
@@ -5,4 +5,19 @@ favorite_languages = {
     'phil': 'python',
 }
 
-print("Sarah's favorite language is " + favorite_languages['sarah'].title() + ".")
+if 'erin' not in favorite_languages.keys():
+        print("Erin, please take our poll!\n")
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        print ("Hi " + name.title() + ", I see your favorite language is " + favorite_languages[name].title()+ "!")
+
+for name in sorted(favorite_languages.keys()):
+    print( name.title() + ", thank you for taking the poll.")
+
+print("\nThe following languages have been mentioned: ")
+for language in set(favorite_languages.values()):
+    print(language.title())
