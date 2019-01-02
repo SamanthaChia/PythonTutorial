970ec71b115c9935f1ea4ef8045c8f4eb1083529
SamanthaChia <samantha1999@live.com.sg>
2019-01-02 14:31:46 +0800

    for loop in a copied list

--- pizza.py
+++ pizza.py
@@ -1,6 +1,13 @@
 pizzas = ['pepperoni', 'hawaiian', 'marinara']
+friend_pizzas = pizzas[:]
 
+pizzas.append('chicken')
+friend_pizzas.append('vegetable')
+
+print("My favorite pizzas are: ")
 for pizza in pizzas:
-    print("I like " + pizza.title() + " pizza. \n")
-
-print("I really love pizza!")
+    print(pizza)
+    
+print("\nMy friend's favorite pizzas are: ")
+for friend_pizza in friend_pizzas:
+    print(friend_pizza)
