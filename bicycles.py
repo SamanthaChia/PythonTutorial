d949f1b275c13c040cd650cdb725485d48eba0c2
SamanthaChia <samantha1999@live.com.sg>
2019-01-02 11:13:15 +0800

    indentation and for loops

--- bicycles.py
+++ bicycles.py
@@ -2,7 +2,7 @@ bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles[0].title())
 
 print(bicycles[-1])
-#the index before trek would be specialized.
+# this will always return the last item in the list.
 
 message = "My first bicycle was a " + bicycles[0].title() + "."
 
