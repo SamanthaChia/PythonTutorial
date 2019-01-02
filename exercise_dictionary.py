190b9dde9f169fdb3962159361bcaed1aac4de4a
SamanthaChia <samantha1999@live.com.sg>
2019-01-02 18:56:38 +0800

    dictionary exercide 6-2

--- exercise_dictionary.py
+++ exercise_dictionary.py
@@ -8,3 +8,17 @@ person = {
 
 print(person)
 
+#6-2 Favorite Numbers
+values = {
+    'sam': 11,
+    'camille': 14,
+    'solaire': 99,
+    'neeko': 0,
+    'yasuo': 666
+}
+
+print(values['sam'])
+print(values['camille'])
+print(values['solaire'])
+print(values['neeko'])
+print(values['yasuo'])
