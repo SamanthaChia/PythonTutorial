038efe9109773ffdbb9c248c54fcbef7eb60ad95
SamanthaChia <samantha1999@live.com.sg>
2019-01-02 14:12:51 +0800

    slicing

---
+++ players.py
@@ -0,0 +1,9 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(players[1:4])
+# print(players[:4])
+# print(players[2:])
+# print(players[-4:]) get the last 4 of the list.
+
+print("Here are the first 3 players on my team: ")
+for player in players[:3]:
+    print(player.title())
