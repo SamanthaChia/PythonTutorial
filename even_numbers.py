9f6e8700006e011ac71dd82c7f511cc2d4595a45
SamanthaChia <samantha1999@live.com.sg>
2019-01-02 11:29:54 +0800

    range function

---
+++ even_numbers.py
@@ -0,0 +1,3 @@
+# starts with the number 2 to 11, it adds 2 repeatedly.
+even_numbers = list(range(2,11,2))
+print(even_numbers)
---
+++ numbers.py
@@ -0,0 +1,6 @@
+# this doesnt print the last number.
+for value in range(1,5):
+    print(value)
+
+numbers = list(range(1,6))
+print(numbers)
