f6caaeeecce1faf262f003946cb0d8701315917a
SamanthaChia <samantha1999@live.com.sg>
2019-01-02 17:37:46 +0800

    uncomment

--- exercise_if3.py
+++ exercise_if3.py
@@ -1,24 +1,24 @@
 #5-8 , 5-9
-# usernames = ['sam', 'kyrx', 'admin', 'user', 'amumu']
+usernames = ['sam', 'kyrx', 'admin', 'user', 'amumu']
 
-# if usernames:
-#     for username in usernames:
-#         if username == 'admin':
-#             print("Hello " + username + ",would you like to see a status report?")
-#         else:
-#             print("Hello " + username + " thank you for logging in again")
-# else:
-#     print("We need to find some users!")
+if usernames:
+    for username in usernames:
+        if username == 'admin':
+            print("Hello " + username + ",would you like to see a status report?")
+        else:
+            print("Hello " + username + " thank you for logging in again")
+else:
+    print("We need to find some users!")
 
 # 5-10 Checking Usernames
-# current_users = ['sam', 'kyrx', 'admin', 'user', 'amumu']
-# new_users = ['sam', 'test', 'leona', 'amumu', 'pyke']
+current_users = ['sam', 'kyrx', 'admin', 'user', 'amumu']
+new_users = ['sam', 'test', 'leona', 'amumu', 'pyke']
 
-# for new_user in new_users:
-#     if new_user in current_users:
-#         print("The username " + new_user + " is already taken! Please enter a new one")
-#     else:
-#         print("The username " + new_user + " is available!")
+for new_user in new_users:
+    if new_user in current_users:
+        print("The username " + new_user + " is already taken! Please enter a new one")
+    else:
+        print("The username " + new_user + " is available!")
 
 #5-11 Ordinal Numbers
 numbers = list(range(1,10))
