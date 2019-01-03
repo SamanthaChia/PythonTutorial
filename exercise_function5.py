039cc47f80ad0cc4ab2bab3901872c24155eb2af
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 18:37:02 +0800

    exercise function 8-12

---
+++ exercise_function5.py
@@ -0,0 +1,7 @@
+#8-12
+def sandwich_items(*food):
+    print(food)
+
+sandwich_items('chicken', 'lettuce', 'tomato')
+sandwich_items('mayo')
+sandwich_items('ketchup', 'cheese')
--- pizza.py
+++ pizza.py
@@ -23,11 +23,11 @@
 # for topping in pizza['toppings']:
 #     print("\t" + topping)
 
-def make_pizza(*toppings):
+def make_pizza(size, *toppings):
     """Summarize the pizza we are about to make. """
-    print("\nMaking a pizza with the following toppings:")
+    print("\nMaking a " + str(size) + "-inch pizza with the following toppings: ")
     for topping in toppings:
         print("- " + topping)
 
-make_pizza('pepperoni')
-make_pizza('mushrooms','green peppers', 'extra cheese')
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms','green peppers', 'extra cheese')
