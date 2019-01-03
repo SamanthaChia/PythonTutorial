3a3dae2c9ea2f205cebba7db0c039bab593695be
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 15:54:01 +0800

    defining a function

--- greeter.py
+++ greeter.py
@@ -1,5 +1,5 @@
-prompt = "If you tell us who you are, we can personalize the messages you see."
-prompt += "\nWhat is your first name?"
-
-name = input(prompt)
-print("\nHello, " + name + "!")
+def greet_user():
+    """Display a simple greeting."""
+    print("Hello!")
+    
+greet_user()
