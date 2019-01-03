ab9a3aa85d347f79b2c0ddae30bbbe9067385849
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 14:55:25 +0800

    exercise input 7-5

--- exercise_input2.py
+++ exercise_input2.py
@@ -1,10 +1,24 @@
 #7-4
-prompt = "\nPlease enter pizza toppings : "
-prompt += "\nEnter 'quit' to end the program."
+# prompt = "\nEnter 'quit' to end the program."
+# prompt += "\nPlease enter pizza toppings : "
+
+# while True:
+#     pizza_topping = input(prompt)
+#     if pizza_topping == 'quit':
+#         break
+#     else:
+#         print("I'll add " + pizza_topping + " on your pizza!")
+
+#7-5
+prompt = "\nEnter 'quit' to end the program."
+prompt += "\nPlease enter your age: "
 
 while True:
-    pizza_topping = input(prompt)
-    if pizza_topping == 'quit':
-        break
+    age = input(prompt)
+    age = int(age)
+    if age < 3:
+        print("The cost of your ticket is $0")
+    elif age <= 12:
+        print("The cost of yout ticket is $10")
     else:
-        print("I'll add " + pizza_topping + " on your pizza!")
+        print("The cost of yout ticket is $15")
