3f23f2484da6a4c06edd4b5c0b4f9ee5a08ff4de
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 18:39:42 +0800

    exercise function 8-13

--- exercise_function5.py
+++ exercise_function5.py
@@ -1,7 +1,20 @@
 #8-12
-def sandwich_items(*food):
-    print(food)
+# def sandwich_items(*food):
+#     print(food)
 
-sandwich_items('chicken', 'lettuce', 'tomato')
-sandwich_items('mayo')
-sandwich_items('ketchup', 'cheese')
+# sandwich_items('chicken', 'lettuce', 'tomato')
+# sandwich_items('mayo')
+# sandwich_items('ketchup', 'cheese')
+
+#8-13
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about a user. """
+    profile = {}
+    profile['first_name'] = first
+    profile['last_name'] = last
+    for key, value in user_info.items():
+        profile[key] = value
+    return profile
+
+user_profile = build_profile('samantha', 'chia', location='singapore',age='20',birthday='11/11/1999')
+print(user_profile)
