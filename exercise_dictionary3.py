e78e4073c52d61226d362f32cb8b7020bd3d923a
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 11:55:59 +0800

    dictionary exercise 6-11

--- exercise_dictionary3.py
+++ exercise_dictionary3.py
@@ -30,3 +30,27 @@ favorite_places = {
 
 # for name,favorite_place in favorite_places.items():
 #     print(name + "'s favorite place is : " + favorite_place)
+
+#6-11
+cities = {
+    'singapore': {
+        'country': 'singapore',
+        'population': 500000,
+        'fact': 'it is a small country'
+    },
+    'akihabara': {
+        'country': 'japan',
+        'population': 100000,
+        'fact': 'it is the land of otakus'
+    },
+    'johor bahru': {
+        'country': 'malaysia',
+        'population' : 200000,
+        'fact': 'it has alot of good food'
+    }
+}
+
+for city_name,city_info in cities.items():
+    print(city_name + " is a city in " + city_info['country'] +
+     " with a population of " + str(city_info['population']) +
+     " and heres one fact : " + city_info['fact'] )
