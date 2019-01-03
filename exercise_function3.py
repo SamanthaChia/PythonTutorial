cf8a372159c27525a01c9ee6357755f740ad1028
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 17:00:55 +0800

    exercise function 8-6

---
+++ exercise_function3.py
@@ -0,0 +1,10 @@
+def city_country(city_name, country):
+    sentance = city_name + ", " + country
+    return sentance
+
+country1 = city_country('Santiago', 'Chile')
+country2 = city_country('Hokkaido', 'Japan')
+country3 = city_country('Sinagapore', 'Singapore')
+print(country1)
+print(country2)
+print(country3)
