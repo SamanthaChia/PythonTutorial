c44f7de6dd2a0eea1561891ea707513ddfc56fd0
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 11:39:00 +0800

    dictionary exercise 6-9

--- exercise_dictionary3.py
+++ exercise_dictionary3.py
@@ -12,11 +12,21 @@ hamster = {
     'owner': 'lucian'
 }
 
+#6-9
 pets = []
 
 pets.append(dog)
 pets.append(cat)
 pets.append(hamster)
 
-for pet in pets:
-    print(pet)
+# for pet in pets:
+#     print(pet)
+
+favorite_places = {
+    'sam': 'japan',
+    'amumu': 'egypt',
+    'owen': 'home'
+}
+
+for name,favorite_place in favorite_places.items():
+    print(name + "'s favorite place is : " + favorite_place)
