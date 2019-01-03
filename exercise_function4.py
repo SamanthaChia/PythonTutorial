313d64abe7654bc8a1543b96fceed72de03dea99
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 17:52:04 +0800

    exercise functions 8-10

--- exercise_function4.py
+++ exercise_function4.py
@@ -1,7 +1,28 @@
+#8-9
+# magicians = ['sam', 'tom', 'tyler', 'phil', 'katie']
+
+# def show_magicians(magicians):
+#     for magician in magicians:
+#         print(magician)
+
+# show_magicians(magicians)
+
+#8-10
 magicians = ['sam', 'tom', 'tyler', 'phil', 'katie']
+great_magicians =[]
+
+
+def make_great(magicians,great_magicians):
+    while magicians:
+        current_magician = magicians.pop()
+        current_magician = "The Great " + current_magician
+        great_magicians.append(current_magician)
 
 def show_magicians(magicians):
     for magician in magicians:
         print(magician)
 
-show_magicians(magicians)
+        
+
+make_great(magicians,great_magicians)
+show_magicians(great_magicians)
