2c533a09ff4f411bb15e1581948d3becdcd2c6a6
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 15:46:53 +0800

    exercise input 7-10

--- exercise_input3.py
+++ exercise_input3.py
@@ -1,15 +1,21 @@
 #7-8,7-9
-sandwich_orders = ['blt', 'chicken', 'pastrami', 'pastrami', 'vegetables', 'pastrami']
-finished_sandwiches = []
-print("We ran out of Pastrami!\n")
-while 'pastrami' in sandwich_orders:
-    sandwich_orders.remove('pastrami')
+# sandwich_orders = ['blt', 'chicken', 'pastrami', 'pastrami', 'vegetables', 'pastrami']
+# finished_sandwiches = []
+# print("We ran out of Pastrami!\n")
+# while 'pastrami' in sandwich_orders:
+#     sandwich_orders.remove('pastrami')
 
-while sandwich_orders:    
-    current_order = sandwich_orders.pop()
-    print("I am currently making a " + current_order.title() + " sandwich.")
+# while sandwich_orders:    
+#     current_order = sandwich_orders.pop()
+#     print("I am currently making a " + current_order.title() + " sandwich.")
 
-    finished_sandwiches.append(current_order)
-print("\n")
-for finished_sandwich in finished_sandwiches:
-    print("I have finished making the " + finished_sandwich + " sandwich.")
+#     finished_sandwiches.append(current_order)
+# print("\n")
+# for finished_sandwich in finished_sandwiches:
+#     print("I have finished making the " + finished_sandwich + " sandwich.")
+
+#7-10
+prompt = "If you could play any game, what would you play?"
+
+question = input(prompt)
+print("I would like to play this game : " + question)
