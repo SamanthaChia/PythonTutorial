ad82fc88365cf29a6fe71817f8ac131d8eecca16
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 16:29:34 +0800

    exercise function 8-4

--- exercise_function2.py
+++ exercise_function2.py
@@ -1,5 +1,6 @@
 #8-3
-def make_shirt(size,message):
+def make_shirt(size='L',message='I love Python!'):
     print("Making a size " + size + " shirt with the message of " + message)
 
-make_shirt('S','hello')
+make_shirt()
+make_shirt(size='M')
