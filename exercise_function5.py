c6ddab52b816888a67634c3761540850802fd953
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 18:53:17 +0800

    exercise function 8-14

--- exercise_function5.py
+++ exercise_function5.py
@@ -7,14 +7,26 @@
 # sandwich_items('ketchup', 'cheese')
 
 #8-13
-def build_profile(first, last, **user_info):
-    """Build a dictionary containing everything we know about a user. """
-    profile = {}
-    profile['first_name'] = first
-    profile['last_name'] = last
-    for key, value in user_info.items():
-        profile[key] = value
-    return profile
+# def build_profile(first, last, **user_info):
+#     """Build a dictionary containing everything we know about a user. """
+#     profile = {}
+#     profile['first_name'] = first
+#     profile['last_name'] = last
+#     for key, value in user_info.items():
+#         profile[key] = value
+#     return profile
 
-user_profile = build_profile('samantha', 'chia', location='singapore',age='20',birthday='11/11/1999')
-print(user_profile)
+# user_profile = build_profile('samantha', 'chia', location='singapore',age='20',birthday='11/11/1999')
+# print(user_profile)
+
+#8-14
+def store_car_info(manufaturer,model_name,**car_info):
+    car = {}
+    car['manufaturer'] = manufaturer
+    car['model_name'] = model_name
+    for key,value in car_info.items():
+        car[key] = value
+    return car
+
+certain_car_info = store_car_info('subaru', 'outback', color='blue', tow_package=True)
+print(certain_car_info)
