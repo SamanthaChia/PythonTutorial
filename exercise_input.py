95967c156ddc9c614fb439db32837c2a312baf96
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 13:52:18 +0800

    exercise input 7-2

--- exercise_input.py
+++ exercise_input.py
@@ -1,3 +1,11 @@
 #7-1
-prompt = input("What kind of rental car would you like?")
-print("Let me see if I can find you a "+ prompt)
+# prompt = input("What kind of rental car would you like?")
+# print("Let me see if I can find you a "+ prompt)
+
+#7-2
+prompt = input("How many people are in your dinner group?")
+prompt = int(prompt)
+if prompt > 8:
+    print("You will need to wait for a table.")
+else:
+    print("Your table is ready")
