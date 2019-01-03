da78a79663777b93327fb5bfbb4c323832db7092
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 16:01:33 +0800

    exercise function 8-2

--- exercise_function.py
+++ exercise_function.py
@@ -1,5 +1,11 @@
 #8-1
-def display_message():
-    print("I am learning how to define a function")
+# def display_message():
+#     print("I am learning how to define a function")
 
-display_message()
+# display_message()
+
+#8-2
+def favorite_book(title):
+    print("One of my favorite books is " + title.title())
+
+favorite_book("dear nobody")
