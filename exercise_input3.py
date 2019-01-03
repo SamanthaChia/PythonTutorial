0e68c7fcf5d9081b8ff80c6c7ad5c93cdc4909c5
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 15:42:02 +0800

    exercise input 7-9

--- exercise_input3.py
+++ exercise_input3.py
@@ -1,12 +1,15 @@
-#7-8
-sandwich_orders = ['BLT', 'Chicken', 'Vegetables']
+#7-8,7-9
+sandwich_orders = ['blt', 'chicken', 'pastrami', 'pastrami', 'vegetables', 'pastrami']
 finished_sandwiches = []
+print("We ran out of Pastrami!\n")
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
 
-while sandwich_orders:
+while sandwich_orders:    
     current_order = sandwich_orders.pop()
     print("I am currently making a " + current_order.title() + " sandwich.")
 
     finished_sandwiches.append(current_order)
-
+print("\n")
 for finished_sandwich in finished_sandwiches:
-    print("\nI have finished making the " + finished_sandwich + " sandwich.")
+    print("I have finished making the " + finished_sandwich + " sandwich.")
