2b6412fe61c1960deccc50d009b3221737520333
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 16:32:57 +0800

    exercise function 8-5

--- exercise_function2.py
+++ exercise_function2.py
@@ -1,6 +1,14 @@
 #8-3
-def make_shirt(size='L',message='I love Python!'):
-    print("Making a size " + size + " shirt with the message of " + message)
+# def make_shirt(size='L',message='I love Python!'):
+#     print("Making a size " + size + " shirt with the message of " + message)
 
-make_shirt()
-make_shirt(size='M')
+# make_shirt()
+# make_shirt(size='M')
+
+#8-5
+def describe_city(city_name,country='japan'):
+    print(city_name + " is in " + country)
+
+describe_city('akihabara')
+describe_city('hokkaido')
+describe_city('singapore','singapore')
