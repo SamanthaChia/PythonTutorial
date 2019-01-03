e8f67a87339d1e5542fe99bec1d2d1e9258c33de
SamanthaChia <samantha1999@live.com.sg>
2019-01-03 13:54:38 +0800

    exercise input 7-3

--- exercise_input.py
+++ exercise_input.py
@@ -3,9 +3,17 @@
 # print("Let me see if I can find you a "+ prompt)
 
 #7-2
-prompt = input("How many people are in your dinner group?")
+# prompt = input("How many people are in your dinner group?")
+# prompt = int(prompt)
+# if prompt > 8:
+#     print("You will need to wait for a table.")
+# else:
+#     print("Your table is ready")
+
+#7-3
+prompt = input("Please provide me a number and I will tell you if it is a multiple of 10 or not :")
 prompt = int(prompt)
-if prompt > 8:
-    print("You will need to wait for a table.")
+if prompt % 10 == 0:
+    print(str(prompt) + " is a multiple of 10")
 else:
-    print("Your table is ready")
+    print(str(prompt) + " is not a multiple of 10")
