1e06ef451d5d00a5077a84e51b6e8e751e882750
SamanthaChia <samantha1999@live.com.sg>
2019-01-08 18:01:26 +0800

    9-11

---
+++ exercise_9-11.py
@@ -0,0 +1,5 @@
+
+from exercise_classes4 import User,Admin,Privileges
+
+new_admin = Admin('sam','chia',100)
+new_admin.privileges.show_privileges()
--- exercise_classes4.py
+++ exercise_classes4.py
@@ -1,18 +1,51 @@
-class Restaurant():
-    def __init__(self, restaurant_name, cuisine_type):
-        self.restaurant_name = restaurant_name
-        self.cuisine_type = cuisine_type
-        self.number_served = 0
+# class Restaurant():
+#     def __init__(self, restaurant_name, cuisine_type):
+#         self.restaurant_name = restaurant_name
+#         self.cuisine_type = cuisine_type
+#         self.number_served = 0
     
-    def describe_restaurant(self):
-        print("The restaurant's name is " + self.restaurant_name)
-        print("The cuisine type is " + self.cuisine_type)
+#     def describe_restaurant(self):
+#         print("The restaurant's name is " + self.restaurant_name)
+#         print("The cuisine type is " + self.cuisine_type)
     
-    def open_restaurant(self):
-        print(self.restaurant_name + " is open!")
+#     def open_restaurant(self):
+#         print(self.restaurant_name + " is open!")
     
-    def set_number_served(self,new_value):
-        self.number_served = new_value
+#     def set_number_served(self,new_value):
+#         self.number_served = new_value
     
-    def increment_number_served(self, increment_value):
-        self.number_served = self.number_served + increment_value
+#     def increment_number_served(self, increment_value):
+#         self.number_served = self.number_served + increment_value
+
+class User():
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.login_attempts = 0
+    
+    def describe_user(self):
+        print("First Name : " + self.first_name)
+        print("Last Name : " + self.last_name)
+        print("Age : " + str(self.age))
+    
+    def greet_user(self):
+        print("Hello , " + self.first_name + " " + self.last_name)
+    
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+    
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+class Privileges():
+    def __init__(self, privileges=['can add post', 'can delete post', 'can ban user']):
+        self.privileges = privileges
+
+    def show_privileges(self):
+        print(self.privileges)
+
+class Admin(User):
+    def __init__(self, first_name, last_name, age):
+        super().__init__(first_name, last_name, age)
+        self.privileges = Privileges()
