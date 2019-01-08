812ac5f8dc054999cc3042108c3edc108af3dafb
SamanthaChia <samantha1999@live.com.sg>
2019-01-08 17:23:05 +0800

    9-8 completed

--- exercise_classes3.py
+++ exercise_classes3.py
@@ -49,13 +49,18 @@ class User():
     def reset_login_attempts(self):
         self.login_attempts = 0
 
-class Admin(User):
-    def __init__(self, first_name, last_name, age):
-        super().__init__(first_name, last_name, age)
-        self.privileges = ['can add post', 'can delete post', 'can ban user']
+class Privileges():
+    def __init__(self, privileges=['can add post', 'can delete post', 'can ban user']):
+        self.privileges = privileges
 
     def show_privileges(self):
         print(self.privileges)
 
+class Admin(User):
+    def __init__(self, first_name, last_name, age):
+        super().__init__(first_name, last_name, age)
+        self.privileges = Privileges()
+
+
 admin1 = Admin('sam', 'chia', '50')
-admin1.show_privileges()
+admin1.privileges.show_privileges()
