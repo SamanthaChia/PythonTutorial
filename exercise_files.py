0c2f42232f32ae69abe9dfb6f162e888886c864a
SamanthaChia <samantha1999@live.com.sg>
2019-01-09 12:15:26 +0800

    10-2 completed

--- exercise_files.py
+++ exercise_files.py
@@ -1,21 +1,24 @@
 filename = 'learning_python.txt'
 
 #Reading the entire file
-with open(filename) as file_object:
-    contents = file_object.read()
-    print(contents)
+# with open(filename) as file_object:
+#     contents = file_object.read()
+#     print(contents)
 
 #Loop over the file object.
 with open(filename) as file_object:
     lines = file_object.readlines()
 
 for line in lines:
-    print(line.rstrip())
+    new_line = line.replace('Python','C')
+    print(new_line.rstrip())
 
 #Store the contents in a list
-with open(filename) as file_object:
-    lines = file_object.readlines()
+# with open(filename) as file_object:
+#     lines = file_object.readlines()
 
-lines_read = ''
-for line in lines:
-    lines_read += line.strip()
+# lines_read = ''
+# for line in lines:
+#     lines_read += line.strip()
+
+
