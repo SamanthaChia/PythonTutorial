315dedd635573de17c21bb25dc76e3e88641cacf
SamanthaChia <samantha1999@live.com.sg>
2019-01-09 12:35:16 +0800

    10-5 completed

--- exercise_files2.py
+++ exercise_files2.py
@@ -4,9 +4,16 @@
 # with open(filename,'w') as file_object:
 #     file_object.write(guest_name)
 
-filename = 'guest_book.txt'
+# filename = 'guest_book.txt'
 
-guest_name = input("Please enter your name : ")
+# guest_name = input("Please enter your name : ")
+# with open(filename,'a') as file_object:
+#     file_object.write(guest_name + "\n")
+#     print("Welcome , " + guest_name)
+
+filename = 'poll.txt'
+
+reason = input("Please enter why you like programming : ")
 with open(filename,'a') as file_object:
-    file_object.write(guest_name + "\n")
-    print("Welcome , " + guest_name)
+    file_object.write(reason + "\n")
+    print("Thank you, your answer has been saved")
