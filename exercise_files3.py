be371fa283aead29f5ef54106e43c9afc7c8bea8
SamanthaChia <samantha1999@live.com.sg>
2019-01-09 15:10:07 +0800

    10-7 completed

--- exercise_files3.py
+++ exercise_files3.py
@@ -1,15 +1,20 @@
-number1 = input("Input the first number :")
-try:
-    number1 = int(number1)
-except ValueError:
-    print("This is not a number!")
+while True:
+    number1 = input("Input the first number :")
+    if number1 == 'q':
+        break
+    try:
+        number1 = int(number1)
+    except ValueError:
+        print("This is not a number!")
 
-number2 = input("Input the second number :")
-try:
-    number2 = int(number2)
-except ValueError:
-    print("This is not a number!")
-total = number1 + number2
-print("The total is : " + str(total))
+    number2 = input("Input the second number :")
+    if number2 == 'q':
+        break
+    try:
+        number2 = int(number2)
+    except ValueError:
+        print("This is not a number!")
+    total = number1 + number2
+    print("The total is : " + str(total))
 
 
