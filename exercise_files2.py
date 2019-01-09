ccf9571479f184a9f881e631dc0b177751814e03
SamanthaChia <samantha1999@live.com.sg>
2019-01-09 12:33:13 +0800

    10-4 completed

--- exercise_files2.py
+++ exercise_files2.py
@@ -1,5 +1,12 @@
-filename = 'guest.txt'
+# filename = 'guest.txt'
+
+# guest_name = input("Please enter your name : ")
+# with open(filename,'w') as file_object:
+#     file_object.write(guest_name)
+
+filename = 'guest_book.txt'
 
 guest_name = input("Please enter your name : ")
-with open(filename,'w') as file_object:
-    file_object.write(guest_name)
+with open(filename,'a') as file_object:
+    file_object.write(guest_name + "\n")
+    print("Welcome , " + guest_name)
